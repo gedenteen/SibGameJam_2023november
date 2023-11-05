fd943b957ddac6f8c6afd1e6c1684b2e963b6638
gedenteen <Razumow.dmitrij@yandex.ru>
2023-11-05 14:14:41 +0300

    Implement saving audio settings

--- Assets/_Project/Scripts/Settings.cs
+++ Assets/_Project/Scripts/Settings.cs
@@ -10,11 +10,42 @@ public class Settings : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private TMPro.TMP_Dropdown resolutionDropdown;
+    [SerializeField] private Slider sliderMusicVolume;
+    [SerializeField] private Slider sliderSoundsVolume;
 
     Resolution[] resolutions;
+    private bool isInitialized = false;
+
+    // Strings for PlayerPrefs
+    private readonly string pp_musicVolume = "MusicVolume";
+    private readonly string pp_soundVolume = "SoundVolume";
+
+    private void Awake()
+    {
+    }
 
     private void Start()
     {
+        // Default values of settings
+        float musicVolume = 1f;
+        float soundVolume = 1f;
+
+        // Get player values, if he change them
+        if (PlayerPrefs.HasKey(pp_musicVolume))
+        {
+            musicVolume = PlayerPrefs.GetFloat(pp_musicVolume);
+        }
+        if (PlayerPrefs.HasKey(pp_soundVolume))
+        {
+            soundVolume = PlayerPrefs.GetFloat(pp_soundVolume);
+        }
+
+        Debug.Log($"Settings: Start: musicVolume={musicVolume} soundVolume={soundVolume}");
+        SetMusicVolume(musicVolume);
+        sliderMusicVolume.value = musicVolume;
+        SetSoundsVolume(soundVolume);
+        sliderSoundsVolume.value = soundVolume;
+
         resolutions = Screen.resolutions;
 
         List<string> options = new List<string>();
@@ -34,6 +65,8 @@ public class Settings : MonoBehaviour
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+
+        isInitialized = true;
     }
 
     public void SetResolution(int resolutionIndex)
@@ -45,11 +78,21 @@ public class Settings : MonoBehaviour
     public void SetMusicVolume(float volume)
     {
         audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+
+        if (isInitialized)
+        {
+            PlayerPrefs.SetFloat(pp_musicVolume, volume);
+        }
     }
 
     public void SetSoundsVolume(float volume)
     {
         audioMixer.SetFloat("SoundsVolume", Mathf.Log10(volume) * 20);
+
+        if (isInitialized)
+        {
+            PlayerPrefs.SetFloat(pp_soundVolume, volume);
+        }
     }
 
     public void SetFullScreen(bool isFullScreen)
