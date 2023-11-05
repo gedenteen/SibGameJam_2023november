5ed34235e7dcd1cbb900461072edc35f59ec4ae8
gedenteen <Razumow.dmitrij@yandex.ru>
2023-11-05 11:07:09 +0300

    Add melody

---
+++ Assets/_Project/Scripts/AudioManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private AudioSource audioSourceMusic;
+    [SerializeField] private AudioClip clipMainMenu;
+
+}
